91d6f8c024e24fed1b2f76d8abbaf103268ff435
victorgarcia2006 <132796314+victorgarcia2006@users.noreply.github.com>
2023-06-30 20:28:37 -0600

    :construction: Edit the calendar

--- apps/frontend/components/OMServices/DateSection.tsx
+++ apps/frontend/components/OMServices/DateSection.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { Group } from '@mantine/core';
 import { Calendar } from '@mantine/dates';
+import dayjs from 'dayjs';
+import { useState } from 'react';
 
 
 export default function DateSection(){
+    const [selected, setSelected] = useState<Date[]>([]);
+    const handleSelect = (date: Date) => {
+      const isSelected = selected.some((s) => dayjs(date).isSame(s, 'date'));
+      if (isSelected) {
+        setSelected((current) => current.filter((d) => !dayjs(d).isSame(date, 'date')));
+      } else if (selected.length < 3) {
+        setSelected((current) => [...current, date]);
+      }
+    };
     return(
         <aside>
             <h6 className="mb-6">Available dates</h6>
             <Calendar
                 style={{ width: 348, paddingRight: 40  }}
                 level="month"
-                defaultLevel="month"
+                defaultLevel="month" 
                 size="md"
                 firstDayOfWeek={0}
                 className="border-solid shadow-md border-[0.1px] border-[#eeeeee] px-5 py-5"
@@ -22,11 +33,17 @@ export default function DateSection(){
                     weekday:{
                         color: "#adb5bd"
                     },
-                    pickerControl:{
-                        color: "#495057",
-                        fontWeight: 700,
+                    calendarHeaderLevel:{
+                        fontWeight: 600,
+                    },
+                    calendarHeaderControl:{
+                        display: "none"
                     }
                 })}
+                getDayProps={(date) => ({
+                    selected: selected.some((s) => dayjs(date).isSame(s, 'date')),
+                    onClick: () => handleSelect(date), 
+                    })}
             />
         </aside>
     );
