abb9fb37ad7c6d8e5741e711956c7c2d5352e32f
victorgarcia2006 <132796314+victorgarcia2006@users.noreply.github.com>
2023-07-08 23:18:01 -0600

    :iphone: Rule Section responsive

--- apps/frontend/components/OMServices/RulesSection.tsx
+++ apps/frontend/components/OMServices/RulesSection.tsx
@@ -42,11 +42,12 @@ const RulesSection = () => {
     <Accordion.Item
       value={item.id}
       key={index}
-      className="w-[760px] rounded-xl bg-[#fff8f6] border-0"
-    
+      className=" md:w-[760px] rounded-xl bg-[#fff8f6] border-0"
     >
       <Accordion.Control className="rounded-xl bg-[#fff8f6] px-8 py-2">
-        <div className="text-xs font-semibold font-montserrat">{item.title}</div>
+        <div className="text-xs font-semibold font-montserrat">
+          {item.title}
+        </div>
       </Accordion.Control>
       <Accordion.Panel className="rounded-xl bg-[#fff8f6] px-4 ">
         <div className="text-justify text-[10px] tracking-wide font-medium leading-4 font-montserrat">
@@ -59,32 +60,34 @@ const RulesSection = () => {
     <section className="w-full h-2/3 bg-primary">
       <Container size="xl" className="pt-2 pb-12">
         <h6 className="mb-6">Rules and FAQ</h6>
-        <div className="flex gap-x-24">
-          {rules.map((rule, index) => (
-            <div key={index} className="flex flex-col gap-y-8">
-              <div className="text-xs text-justify tracking-wide font-bold">
-                {rule.title}
+        <div className="flex flex-col lg:flex-row gap-y-10 gap-x-24">
+          <div className="flex flex-col md:flex-row gap-y-10 gap-x-24">
+            {rules.map((rule, index) => (
+              <div key={index} className="flex flex-col gap-y-8">
+                <div className="text-xs text-justify tracking-wide font-bold">
+                  {rule.title}
+                </div>
+                <div className="flex flex-col text-xs text-justify tracking-wide font-medium gap-y-2">
+                  <div>{rule.rule1}</div>
+                  <div>{rule.rule2}</div>
+                  <div>{rule.rule3}</div>
+                </div>
               </div>
-              <div className="flex flex-col text-xs text-justify tracking-wide font-medium gap-y-2">
-                <div>{rule.rule1}</div>
-                <div>{rule.rule2}</div>
-                <div>{rule.rule3}</div>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
           <div>
             <div className="text-xs text-justify tracking-wide font-bold mb-8">
               FAQ
             </div>
-            <Accordion 
-              variant="separated" 
-              className="" 
+            <Accordion
+              variant="separated"
+              className=""
               defaultValue="lorem1"
               chevron={<ChevronRight size="1rem" />}
-              styles = {{
+              styles={{
                 chevron: {
-                  '&[data-rotate]': {
-                    transform: 'rotate(90deg)'
+                  "&[data-rotate]": {
+                    transform: "rotate(90deg)",
                   },
                 },
               }}
