1563c4b14fbd379f155a763e85efb0d9b70557ca
Alexis Herrera Saucedo <47467314+AlexisHS458@users.noreply.github.com>
2023-08-28 21:29:43 -0600

    reviews

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 import {
-  AppShell,
-  Navbar,
-  Header,
-  Footer,
-  Aside,
-  Text,
-  MediaQuery,
-  Burger,
-  useMantineTheme,
-  Box,
-  BackgroundImage,
-  Center,
-  Container,
-  Grid,
-  Button,
-  Card,
+	AppShell,
+	Navbar,
+	Header,
+	Footer,
+	Aside,
+	Text,
+	MediaQuery,
+	Burger,
+	useMantineTheme,
+	Box,
+	BackgroundImage,
+	Center,
+	Container,
+	Grid,
+	Button,
+	Card,
 } from "@mantine/core";
 import Appbar from "../components/header";
 import HeroSection from "../components/Home/Hero";
@@ -31,47 +31,47 @@ import { ListCatalogResponse, SearchCatalogItemsResponse } from "square";
 import { GetServerSideProps } from "next";
 
 const API_BASE_URL =
-  "https://ourmaids-website-frontend-git-alexis-ocstudios.vercel.app/api";
+	"https://ourmaids-website-frontend-git-alexis-ocstudios.vercel.app/api";
 import CatalogService from "../services/catalog.service";
 import ImagesService from "../services/images.service";
 interface ServicesProps {
-  services: any;
-  reviewsGoogle: any;
+	services: any;
+	reviewsGoogle: any;
 }
 interface ReviewsProps {
-  reviewsGoogle: any;
+	reviewsGoogle: any;
 }
 enum CatalogItemProductType {
-  AppointmentsService = "APPOINTMENTS_SERVICE",
-  Regular = "REGULAR",
+	AppointmentsService = "APPOINTMENTS_SERVICE",
+	Regular = "REGULAR",
 }
 import ReCAPTCHA from "react-google-recaptcha";
 import {
-  GoogleReCaptcha,
-  GoogleReCaptchaProvider,
+	GoogleReCaptcha,
+	GoogleReCaptchaProvider,
 } from "react-google-recaptcha-v3";
 export default function AppShellDemo({
-  services,
-  reviewsGoogle,
+	services,
+	reviewsGoogle,
 }: ServicesProps) {
-  /* console.log(reviewsGoogle); */
+	/* console.log(reviewsGoogle); */
 
-  const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
-  const reviews = [
-    {
-      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      image: "/images/hero.png",
-      rating: 5,
-    },
-  ];
-  const data = services;
-  /* console.log(data); */
+	const theme = useMantineTheme();
+	const [opened, setOpened] = useState(false);
+	const reviews = [
+		{
+			title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+			description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+			image: "/images/hero.png",
+			rating: 5,
+		},
+	];
+	const data = services;
+	/* console.log(data); */
 
-  return (
-    <>
-      {/*    <HeroSection />
+	return (
+		<>
+			{/*    <HeroSection />
       <Container
         style={{
           marginTop: "200px",
@@ -83,76 +83,76 @@ export default function AppShellDemo({
       </Container>
     
     */}
-      <HeroSection />
-      <Container className="my-16  lg:my-52" size="xl">
-        <OurServices data={data} />
-      </Container>
-      <AboutUs />
-      <Booking />
-      <Reviews reviewsGoogle={reviewsGoogle} />
-      {/* 	<ReCAPTCHA
+			<HeroSection />
+			<Container className="my-16  lg:my-52" size="xl">
+				<OurServices data={data} />
+			</Container>
+			<AboutUs />
+			<Booking />
+			<Reviews reviewsGoogle={reviewsGoogle} />
+			{/* 	<ReCAPTCHA
 				sitekey="6Ld61rQnAAAAAOZyssOajwm8AsrA6CEAGzRcpcs4"
 				onChange={(value) => {
 					console.log("Captcha value:", value);
 				}}
 			/> */}
-      {/* <GoogleReCaptchaProvider reCaptchaKey="6LdrxLQnAAAAAGbBzf5OvpkFr-Gc0FD2RN9YFMod">
+			{/* <GoogleReCaptchaProvider reCaptchaKey="6LdrxLQnAAAAAGbBzf5OvpkFr-Gc0FD2RN9YFMod">
 				<GoogleReCaptcha
 					onVerify={(token) => {
 						console.log("Captcha value:", token);
 					}}
 				/>
 			</GoogleReCaptchaProvider> */}
-      ,
-    </>
-  );
+			,
+		</>
+	);
 }
 
 function enhanceCatalogData(
-  catalogData: SearchCatalogItemsResponse,
-  imagesData: ListCatalogResponse
+	catalogData: SearchCatalogItemsResponse,
+	imagesData: ListCatalogResponse
 ) {
-  return catalogData.items?.map((item) => {
-    const image = imagesData.objects?.find(
-      (image) =>
-        image.type === "IMAGE" &&
-        image.id === (item.itemData?.imageIds?.[0] || "")
-    );
+	return catalogData.items?.map((item) => {
+		const image = imagesData.objects?.find(
+			(image) =>
+				image.type === "IMAGE" &&
+				image.id === (item.itemData?.imageIds?.[0] || "")
+		);
 
-    return {
-      ...item,
-      imageData: image?.imageData || null,
-    };
-  });
+		return {
+			...item,
+			imageData: image?.imageData || null,
+		};
+	});
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  try {
-    const [catalogData, imagesData] = await Promise.all([
-      CatalogService.fetchCatalogItems(CatalogItemProductType.Regular, 10),
-      ImagesService.fetchImages(),
-    ]);
+	try {
+		const [catalogData, imagesData] = await Promise.all([
+			CatalogService.fetchCatalogItems(CatalogItemProductType.Regular, 10),
+			ImagesService.fetchImages(),
+		]);
 
-    const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${"ChIJC-g-TsaVwoAReX0svaIhOL4"}&key=${"AIzaSyCFIeOK8R5tCOSIPBDeCceJx-ayHwwXfhw&"}&sessiontoken=${""}`;
+		const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${"ChIJC-g-TsaVwoAReX0svaIhOL4"}&key=${"AIzaSyCFIeOK8R5tCOSIPBDeCceJx-ayHwwXfhw&"}`;
 
-    const enhancedCatalogData = enhanceCatalogData(catalogData, imagesData);
-    const reviews = await fetch(url);
-    const data = await reviews.json();
-    console.log(data);
+		const enhancedCatalogData = enhanceCatalogData(catalogData, imagesData);
+		const reviews = await fetch(url);
+		const data = await reviews.json();
+		console.log(data);
 
-    return {
-      props: {
-        services: enhancedCatalogData,
-        reviewsGoogle: data && data.status === "OK" ? data.result : null,
-      },
-    };
-  } catch (error) {
-    console.error("Error:", error);
+		return {
+			props: {
+				services: enhancedCatalogData,
+				reviewsGoogle: data && data.status === "OK" ? data.result : null,
+			},
+		};
+	} catch (error) {
+		console.error("Error:", error);
 
-    return {
-      props: {
-        services: null,
-      },
-    };
-  }
+		return {
+			props: {
+				services: null,
+			},
+		};
+	}
 };
