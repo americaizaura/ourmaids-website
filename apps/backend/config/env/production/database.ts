3c3984ae24de9543b229daf0c3e84083f75397e1
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2023-09-12 17:54:21 -0600

    database production

---
+++ apps/backend/config/env/production/database.ts
@@ -0,0 +1,31 @@
+import { parseUri } from 'mysql-parse';
+
+export default ({ env }) => {
+    try {
+        const { host, port, database, user, password } = parseUri(env('DATABASE_URL'));
+
+        return {
+            connection: {
+                client: 'mysql',
+                connection: {
+                    host,
+                    port,
+                    database,
+                    user,
+                    password,
+                    ssl: env('DATABASE_CA')
+                        ? {
+                              ca: env('DATABASE_CA'),
+                          }
+                        : false,
+                },
+                debug: false,
+            },
+        };
+    } catch (e) {
+        throw new Error(`
+        Database ENV = ${env('DATABASE_URL')}
+        All Envs = ${JSON.stringify(process.env)}
+        `);
+    }
+};
