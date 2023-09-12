7f50fe15ec8ba764c4457a2c96c3a1e117c5d7ee
Alexis Herrera Saucedo <47467314+AlexisHS458@users.noreply.github.com>
2023-09-11 22:38:31 -0600

    :sparkles: google meta tags

--- apps/frontend/pages/_document.tsx
+++ apps/frontend/pages/_document.tsx
@@ -11,38 +11,13 @@ export default class _Document extends Document {
 			<Html lang="en-US">
 				<Head>
 					<link rel="shortcut icon" href="/favicon.ico" />
-					<Script id="google-tag-manager" strategy="afterInteractive">
-						{`
-        					(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-							})(window,document,'script','dataLayer','GTM-5FHW3ZM');
-						`}
-					</Script>
-					<Script
-						id="google-tag-manager-body"
-						strategy="afterInteractive"
-						dangerouslySetInnerHTML={{
-							__html: `window.dataLayer = window.dataLayer || [];
-							function gtag(){dataLayer.push(arguments);}
-							gtag('js', new Date());
-						  
-							gtag('config', 'AW-363431816');`,
-						}}
-					></Script>
-					<Script
-						strategy="afterInteractive"
-						src={`https://www.googletagmanager.com/gtag/js?id=AW-363431816`}
-					/>
 				</Head>
 				<body>
 					<noscript
 						dangerouslySetInnerHTML={{
-							__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FHW3ZM"
-							height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+							__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FHW3ZM" height="0" width="0" style="display: none; visibility: hidden;" />`,
 						}}
-					></noscript>
+					/>
 					<Main />
 					<NextScript />
 				</body>
